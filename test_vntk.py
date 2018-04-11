a662238ea44758bacce9e09e90280bb4d14ed489
Duc Do <dotrananhduc@gmail.com>
2018-04-11 08:10:10 +0700

    test file

---
+++ test_vntk.py
@@ -0,0 +1,11 @@
+from vntk import vnnews
+
+# test = vnnews()
+
+print(vnnews().raws())
+# print(vnnews().fileids())
+# print(test.words())
+# print(test.tagged_words())
+# print(test.words().concordance('thông_tin'))
+# print(test.words().similar('ngâm'))
+# print(test.words().common_contexts(['ngâm', 'đau']))
